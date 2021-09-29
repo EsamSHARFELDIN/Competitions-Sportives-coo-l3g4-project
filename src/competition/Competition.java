904d673317ffcb3175f47269ba46c1a133596e0d
traorea <traorea@a16p21.fil.univ-lille1.fr>
2021-09-29 09:35:08 +0200

    project

--- src/competition/Competition.java
+++ src/competition/Competition.java
@@ -13,13 +13,9 @@ public abstract class Competition {
 	private Match match;
 	private  final List<Competitor> competitors;
 	
-
-
-	
 	public Competition(List<Competitor> competitors) {
 		this.competitors = competitors;
 	}
-
 
 	public Match getMatch() {
 		return match;
@@ -29,35 +25,21 @@ public abstract class Competition {
 		this.match = match;
 	}
 
-
 	public List<Competitor> getCompetitors() {
 		return competitors;
 	}
-	/*public play() {	
+	public void play() {
+		System.out.println("method play");
+	}
+	
+	protected  abstract List<Competitor> play(List<Competitor> competitors);
+	
+	
+	protected void playMatch(Competitor c1,Competitor c2) {
 		
 	}
-	protected  play(List<Competitor> competitors) {
-		this.competitors = competitors;
-	}
-	protected playMatch(Competitor c1, Competitor c2) {
-		
-		if (c1.getName().equals("bye"))		
-			c = 0;					
-		else if (c2.getName().equals("bye"))	
-			c = 1;					
-		
-		if (c > 0) {		
-			winner = c1;
-			c1.addWin();
-			loser = c2;
-			c2.addLoss();
-		} else {		
-			winner = c2;
-			c2.addWin();
-			loser = c1;
-			c1.addLoss();
-		}
-		return winner;
-	}*/
-
+	
+	
+	
+	
 }
