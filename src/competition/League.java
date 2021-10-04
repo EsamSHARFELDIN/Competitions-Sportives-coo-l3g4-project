ed2ca287f1a77e84afeff299c1ead795a3c39f1f
traorea <traorea@a10p2.fil.univ-lille1.fr>
2021-10-04 16:35:29 +0200

    first commit

--- src/competition/League.java
+++ src/competition/League.java
@@ -1,5 +1,12 @@
 package competition;
 
+import java.util.List;
+
 public class League extends Competition {
+
+	public League(List<Competitor> competitors) {
+		super(competitors);
+		// TODO Auto-generated constructor stub
+	}
 
 }
--- src/competition/Tournament.java
+++ src/competition/Tournament.java
@@ -1,5 +1,12 @@
 package competition;
 
+import java.util.List;
+
 public class Tournament extends Competition {
+
+	public Tournament(List<Competitor> competitors) {
+		super(competitors);
+		// TODO Auto-generated constructor stub
+	}
 
 }
